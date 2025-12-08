1d17cfeb3f8bd990f6acc126c3d3cac1821b6611
reyvandito <147903378+reyvandito@users.noreply.github.com>
2025-12-08 16:12:23 +0700

    Reyvan pacar dinar

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('index'); // pastikan punya resources/views/index.blade.php
+})->name('home');
+
+Route::get('/detail-badminton', function () {
+    return view('detail-badminton');
 });
+
+Route::get('/basket', function () {
+    return view('basket');
+});
+
+Route::get('/sepakbola', function () {
+    return view('sepakbola');
+});
+
